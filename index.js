dc6fcaed7e405bb02e573689d279329674bded4d
Andriy323 <108237550+Andriy323@users.noreply.github.com>
2023-03-03 17:33:11 +0200

    correct error

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
-const dino = document.querySelector("#dino")
-const cactus = document.querySelector('#cactus')
-document.addEventListener("keydown", function(event){
-    jumpDino()
-})
+const dino = document.querySelector("#dino");
+const cactus = document.querySelector("#cactus");
+const x = document.querySelector("#pretitle");
+const record = document.querySelector("#record");
+document.addEventListener("keydown", function (event) {
+  jumpDino();
+});
 
-function jumpDino ()  {
-if(dino.classList !== "jump-dino"){
-    dino.classList.add("jump-dino")
-
+function jumpDino() {
+  if (dino.classList !== "jump-dino") {
+    dino.classList.add("jump-dino");
+  }
+  setTimeout(function () {
+    dino.classList.remove("jump-dino");
+  }, 300);
 }
-setTimeout(function(){
-    dino.classList.remove("jump-dino")
+let y = 0;
+let records = 0;
+let gameOver = setInterval(function () {
+  let dinoData = parseInt(
+    window.getComputedStyle(dino).getPropertyValue("top")
+  );
+  let cactusData = parseInt(
+    window.getComputedStyle(cactus).getPropertyValue("left")
+  );
+  if (cactusData < 70 && cactusData > 0 && dinoData >= 615) {
+    y = 0;
 
-},300)
-}
-
-let gameOver = setInterval(function (){
-    let dinoData = parseInt(window.getComputedStyle(dino).getPropertyValue('top'))
-    let cactusData = parseInt(window.getComputedStyle(cactus).getPropertyValue('left'))
-if(cactusData < 70 && cactusData > 0 && dinoData >= 210){
-return alert("Ooops... Game over")
-}
-},10)
+    return alert("Ooops... Game over");
+  } else {
+    y += 1;
+    records = records < y ? y : records;
+    x.textContent = `Start: ${y}`;
+    record.textContent = `Record: ${records}`;
+  }
+}, 30);
